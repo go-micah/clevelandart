c1862994d7f66ce427aa722911dff69b730daa50
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-01-30 01:37:28 +0000

    get artweork by ID

--- cmd/artwork.go
+++ cmd/artwork.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/go-micah/clevelandart"
 )
 
 func main() {
-	art, _ := clevelandart.GetArtworkByID("162080")
-	fmt.Println(*art)
+
+	args := os.Args[1:]
+	artworkID := args[0]
+
+	art, err := clevelandart.GetArtworkByID(artworkID)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(*art)
+	}
 }
